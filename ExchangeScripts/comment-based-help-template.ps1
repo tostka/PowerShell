2fbe1b5eadc08ff23ba870ecb76d5933ed74c57f
Tostka <tostka@users.noreply.github.com>
2018-03-29 17:31:41 -0500

    comment-based-help-template.ps1
    5:31 PM 3/29/2018 - posting a copy of an old 2013 template to Github, as
    part of shifting in-line code to a gist.

---
+++ ExchangeScripts/comment-based-help-template.ps1
@@ -0,0 +1,40 @@
+#*----------V Comment-based Help (leave blank line below) V---------- 
+
+<# 
+.SYNOPSIS
+[NAME].ps1 - [1-LINE-DESC]
+
+.NOTES
+Written By: Todd Kadrie
+Website: http://tinstoys.blogspot.com
+Twitter: http://twitter.com/tostka
+
+Additional Credits: [REFERENCE]
+Website: [URL]
+Twitter: [URL]
+
+Change Log
+[VERSIONS]
+
+.DESCRIPTION
+[DESC]
+
+.PARAMETER  <Parameter-Name>
+<parameter comment>
+
+.INPUTS
+None. Does not accepted piped input.
+
+.OUTPUTS
+None. Returns no objects or output.
+System.Boolean
+          True if the current Powershell is elevated, false if not.
+[use a | get-member on the script to see exactly what .NET obj TypeName is being returning for the info above]
+
+.EXAMPLE
+.\[SYNTAX EXAMPLE]
+[use an .EXAMPLE keyword per syntax sample]
+
+.LINK
+< name of a related topic, one .LINK keyword per related topic. Can also include a URI to online help (start with http:// or https://)>
+*----------^ END Comment-based Help  ^---------- #>
